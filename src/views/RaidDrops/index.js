0e1e8b06f4ffe590f85b72675e3e58f68efd0507
Josh Hunt <josh@trtr.co>
2017-01-22 11:14:12 +1100

    fix

--- src/views/RaidDrops/index.js
+++ src/views/RaidDrops/index.js
@@ -105,7 +105,7 @@ class RaidDrops extends Component {
   }
 
   updateState() {
-    if (!(this.destinyAccount && this.inventory && this.activityData)) {
+    if (!this.activityData) {
       return;
     }
 
