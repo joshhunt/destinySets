a8d63d26843f3fcddfc82e1eca3ac6af2e89d8ca
Josh Hunt <josh@trtr.co>
2017-04-01 11:23:07 +1100

    Fixed auth bug and activity thing

--- src/components/Activity/index.js
+++ src/components/Activity/index.js
@@ -34,7 +34,7 @@ export default function Activity(props) {
         : null
       }
 
-      { sections.length ? sections.map(section => (
+      { (sections && sections.length) ? sections.map(section => (
         <div className={styles.section} key={section.id}>
           <div className={styles.sectionName}>{section.title}</div>
           {section.items.map(item => <Item key={item.itemHash} item={item} small />)}
--- src/lib/destinyAuth.js
+++ src/lib/destinyAuth.js
@@ -24,6 +24,14 @@ function handleNewAuthData(data) {
   return Promise.resolve();
 }
 
+function handleError(err, cb) {
+  console.error('Auth error:');
+  console.error(err);
+  localStorage.setItem('oldAuthData', localStorage.getItem('authData'));
+  localStorage.removeItem('authData');
+  cb(err);
+}
+
 export default function(cb) {
   const queryParams = queryString.parse(location.search);
 
@@ -43,13 +51,20 @@ export default function(cb) {
     cb(null, true);
 
   } else if (!accessTokenIsValid && refreshTokenIsValid) {
-    console.info('Do refresh logic here');
+    console.info('Access token has expired, but refresh token is still valid.');
+    console.info('Using refresh token to get a new access token');
 
     getDestiny('https://www.bungie.net/Platform/App/GetAccessTokensFromRefreshToken/', {}, {
       refreshToken: prevAuthData.refreshToken,
     }).then(handleNewAuthData)
-      .then(() => cb(null, true))
-      .catch(cb);
+      .then(() => {
+        console.info('Successfully gotten new access token');
+        cb(null, true);
+      })
+      .catch((err) => {
+        console.info('Failed to get new access token');
+        handleError(err, cb);
+      })
 
   } else if (queryParams.code) {
     window.history.replaceState( {} , 'foo', '/' );
@@ -59,7 +74,7 @@ export default function(cb) {
       code: queryParams.code,
     }).then(handleNewAuthData)
       .then(() => cb(null, true))
-      .catch(cb);
+      .catch(err => handleError(err, cb));
 
   } else {
     // console.info('Requesting authorization from Bungie');
