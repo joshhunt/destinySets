cf2c2b84750b40abac4f1ce1716041844e294d3d
Josh Hunt <josh@trtr.co>
2017-10-08 23:48:49 +0100

    Minor changes

--- src/components/LoginUpsell/index.js
+++ src/components/LoginUpsell/index.js
@@ -7,11 +7,13 @@ import styles from './styles.styl';
 export default function LoginUpsell({ children }) {
   return (
     <div className={styles.loginUpsell}>
-      <h2 className={styles.heading}>Login for more features</h2>
+      <h2 className={styles.heading}>Login for more</h2>
 
       {children && <p>{children}</p>}
 
-      <a className={styles.authLink} href={authUrl}>Authorize with Bungie.net</a>
+      <a className={styles.authLink} href={authUrl}>
+        Authorize with Bungie.net
+      </a>
     </div>
   );
 }
