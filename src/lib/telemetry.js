56d8aa12f4571937cff74e2eecbe20aaf7a092e5
Josh Hunt <josh@trtr.co>
2017-10-14 11:16:11 +0100

    Fix telemetry failing if item has no sockets
    Also re-wrap the whole thing in try/catch

--- src/lib/telemetry.js
+++ src/lib/telemetry.js
@@ -23,19 +23,19 @@ function saveInventoryToFirebase(profileId, inventory, ghosts) {
 }
 
 export function saveInventory(profile, inventory) {
-  const profileId = profile.profile.data.userInfo.membershipId;
-  const ghosts = destiny
-    .collectItemsFromProfile(this.profile, true)
-    .filter(item => item.bucketHash === GHOST_BUCKET) // dodgy way to get all ghosts
-    .map(item => {
-      return {
-        itemHash: item.itemHash,
-        itemInstanceId: item.itemInstanceId,
-        sockets: item.$sockets.sockets.map(s => s.plugHash).filter(Boolean),
-      };
-    });
-
   try {
+    const profileId = profile.profile.data.userInfo.membershipId;
+    const ghosts = destiny
+      .collectItemsFromProfile(profile, true)
+      .filter(item => item.bucketHash === GHOST_BUCKET && item.$sockets) // dodgy way to get all ghosts
+      .map(item => {
+        return {
+          itemHash: item.itemHash,
+          itemInstanceId: item.itemInstanceId,
+          sockets: item.$sockets.sockets.map(s => s.plugHash).filter(Boolean),
+        };
+      });
+
     saveInventoryToFirebase(profileId, inventory, ghosts);
   } catch (e) {
     console.error('Error with inventory telemetry');
