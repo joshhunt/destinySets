93968687d8824ba7fb437da2ca20f680f468b67d
Josh Hunt <josh@trtr.co>
2017-10-17 13:07:45 +0100

    Add Borealis to PS4 exlcusive list

--- src/views/consoleExclusives.js
+++ src/views/consoleExclusives.js
@@ -1,19 +1,20 @@
 module.exports = {
   ps4: [
-    155832748,
-    3646674533,
-    3066593211,
-    3569443559,
-    1847870034,
-    690335398,
-    3999262583,
-    3291075521,
-    1512829977,
-    4073580572,
-    2811180959,
-    3169402598,
-    4092393610,
-    2728535008,
-    434243995,
+    155832748, // Icarus Drifter Mask
+    1847870034, // Icarus Drifter Cape
+    3066593211, // Icarus Drifter Vest
+    3569443559, // Icarus Drifter Legs
+    3646674533, // Icarus Drifter Grips
+    690335398, // Terra Concord Helm
+    1512829977, // Terra Concord Greaves
+    3291075521, // Terra Concord Plate
+    3999262583, // Terra Concord Fists
+    4073580572, // Terra Concord Mark
+    2728535008, // Tesseract Trace IV
+    2811180959, // Tesseract Trace IV
+    3169402598, // Tesseract Trace IV
+    4092393610, // Tesseract Trace IV
+    434243995, // Hodiocentrist Bond
+    3141979347, // Borealis
   ],
 };
