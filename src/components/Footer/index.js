37d8401e6352bce28758d09e93ba6c9ac6441ec8
Josh Hunt <josh@trtr.co>
2017-10-25 23:30:51 +0100

    Add Github link in footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -11,7 +11,11 @@ export default function Footer({ item }) {
       </a>{' '}
       for Destiny fans. <br />
       Having issues or need help?{' '}
-      <a href="https://twitter.com/joshhunt">Contact me on twitter</a> <br />
+      <a href="https://twitter.com/joshhunt">Contact me on twitter</a> or{' '}
+      <a href="https://github.com/joshhunt/destinySets">
+        file an issue on Github
+      </a>{' '}
+      <br />
       Destiny is a registered trademark of Bungie. Data and images sourced from
       Bungie.
     </div>
