891e00b0463afdbfcd4195fd5434b646a6fa15fa
Josh Hunt <josh@trtr.co>
2018-04-04 15:54:02 +0100

    Background colour for itemes
    Useful for when offline and item images don't load.

--- src/components/NewItem/index.js
+++ src/components/NewItem/index.js
@@ -1,9 +1,48 @@
 import React, { Component } from 'react';
 import cx from 'classnames';
 
+import {
+  LEGENDARY,
+  EXOTIC,
+  UNCOMMON,
+  RARE,
+  COMMON,
+  EMBLEM
+} from 'app/lib/destinyEnums';
 import Icon from 'app/components/Icon';
 
 import styles from './styles.styl';
+
+const TIER_COLOR = {
+  [EXOTIC]: '#ceae33',
+  [LEGENDARY]: '#522f65',
+  [UNCOMMON]: '#366f3c',
+  [RARE]: '#5076a3',
+  [COMMON]: '#c3bcb4'
+};
+
+function getItemColor(item) {
+  if (!item) {
+    return null;
+  }
+
+  const { red, green, blue } = item.backgroundColor || {
+    red: 0,
+    green: 0,
+    blue: 0
+  };
+  const luminosity = red + green + blue;
+  if (
+    item.itemCategoryHashes.includes(EMBLEM) &&
+    luminosity > 10 &&
+    luminosity < 735
+  ) {
+    return `rgb(${red}, ${green}, ${blue})`;
+  } else {
+    // use rarity color
+    return TIER_COLOR[item.inventory.tierTypeHash];
+  }
+}
 
 export default class Item extends Component {
   onMouseEnter = () => {
@@ -27,9 +66,15 @@ export default class Item extends Component {
 
   render() {
     const { className, item, inventoryEntry } = this.props;
+    const bgColor = getItemColor(item);
 
     if (!item) {
-      return <div className={cx(className, styles.placeholder)} />;
+      return (
+        <div
+          className={cx(className, styles.placeholder)}
+          style={{ backgroundColor: bgColor }}
+        />
+      );
     }
 
     const icon = item.displayProperties.icon || '/img/misc/missing_icon_d2.png';
@@ -50,6 +95,7 @@ export default class Item extends Component {
         <img
           src={`https://www.bungie.net${icon}`}
           className={styles.image}
+          style={{ backgroundColor: bgColor }}
           alt=""
         />
 
