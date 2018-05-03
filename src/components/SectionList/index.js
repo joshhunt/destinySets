930450e8cd55134a6f3a6a1acc211f9fb8132677
Josh Hunt <josh@trtr.co>
2018-05-03 14:09:30 +0200

    Switch to react-scroll

--- src/components/SectionList/index.js
+++ src/components/SectionList/index.js
@@ -1,34 +1,37 @@
 import React from 'react';
 import Scrollspy from 'react-scrollspy';
+import {
+  Link,
+  Element,
+  Events,
+  animateScroll as scroll,
+  scrollSpy,
+  scroller
+} from 'react-scroll';
 import { memoize } from 'lodash';
 
 import styles from './styles.styl';
 
-const getIds = memoize(setData => {
-  console.log('memoize setData is', setData);
-  return setData.map(({ slug }) => slug);
-});
+const getIds = memoize(setData => setData.map(({ slug }) => slug));
 
 export default function SectionList({ setData }) {
-  console.log('setData is', setData);
-  if (!setData) {
-    return <h2>no setData uyet</h2>;
-  }
   return (
     <div className={styles.root}>
       <div>
-        <Scrollspy
-          offset={-100}
-          items={getIds(setData)}
-          className={styles.list}
-          currentClassName={styles.itemActive}
-        >
+        <ul className={styles.list}>
           {setData.map(({ name, slug }) => (
-            <li className={styles.item}>
-              <a href={`#${slug}`}>{name}</a>
-            </li>
+            <Link
+              spy={true}
+              smooth={true}
+              to={slug}
+              offset={-100}
+              className={styles.item}
+              activeClass={styles.itemActive}
+            >
+              {name}
+            </Link>
           ))}
-        </Scrollspy>
+        </ul>
       </div>
     </div>
   );
