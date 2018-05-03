79077c443eb0dc60eeba2ccca28a23e411634dc8
Josh Hunt <josh@trtr.co>
2018-05-03 13:29:10 +0200

    WIP

--- src/views/Inventory/index.js
+++ src/views/Inventory/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Scrollspy from 'react-scrollspy';
 
 import {
   setProfiles,
@@ -282,6 +283,37 @@ class Inventory extends Component {
             collected and dismantled.
           </LoginUpsell>
         )}
+
+        <div>
+          <Scrollspy
+            offset={-60}
+            items={['section-1', 'section-2', 'section-3']}
+            className={styles.testNav}
+            currentClassName={styles.testNavCurrent}
+          >
+            <li>
+              <a href="#section-1">section 1</a>
+            </li>
+            <li>
+              <a href="#section-2">section 2</a>
+            </li>
+            <li>
+              <a href="#section-3">section 3</a>
+            </li>
+          </Scrollspy>
+
+          <div>
+            <section className={styles.testSection} id="section-1">
+              section 1
+            </section>
+            <section className={styles.testSection} id="section-2">
+              section 2
+            </section>
+            <section className={styles.testSection} id="section-3">
+              section 3
+            </section>
+          </div>
+        </div>
 
         <FilterBar filters={filters} toggleFilter={this.toggleFilter} />
 
