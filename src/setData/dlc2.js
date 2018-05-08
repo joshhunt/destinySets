58acf04325d5cdbe1441ce24efedb0edc4f79429
Josh Hunt <josh@trtr.co>
2018-05-08 17:11:17 +0200

    Include scaffold for DLC2 Warmind/S3

---
+++ src/setData/dlc2.js
@@ -0,0 +1,285 @@
+module.exports = [
+  {
+    name: 'Endgame',
+    sets: [
+      {
+        name: 'Spire of Stars, Raid Lair',
+        description:
+          'Gear obtained by playing the Spire of Stars raid lair and increasing your reputation with Emperor Calus.',
+        sections: [
+          {
+            name: 'Weapons',
+            items: [0]
+          },
+
+          {
+            name: 'Hunter Armor',
+            items: [0]
+          },
+          {
+            name: 'Titan Armor',
+            items: [0]
+          },
+          {
+            name: 'Warlock Armor',
+            items: [0]
+          },
+          {
+            name: 'Extras',
+            items: [0]
+          }
+        ]
+      },
+      {
+        name: 'Iron Banner',
+        description:
+          'Weapons and armor obtained by honoring the heroes of old in the Iron Banner Crucible tournament.',
+        sections: [
+          {
+            name: 'Weapons',
+            items: [0]
+          },
+          {
+            name: 'Hunter Armor',
+            items: [0]
+          },
+          {
+            name: 'Titan Armor',
+            items: [0]
+          },
+          {
+            name: 'Warlock Armor',
+            items: [0]
+          },
+          {
+            name: 'Extras',
+            items: [0]
+          }
+        ]
+      },
+      {
+        name: 'Trials of the Nine',
+        description:
+          'Weapons and armor obtained by competing in the Trials of the Nine.',
+        sections: [
+          {
+            name: 'Weapons',
+            items: [0]
+          },
+          {
+            name: 'Hunter Ornaments',
+            items: [0]
+          },
+          {
+            name: 'Hunter Flawless Ornaments',
+            items: [0]
+          },
+          {
+            name: 'Titan Ornaments',
+            items: [0]
+          },
+          {
+            name: 'Titan Flawless Ornaments',
+            items: [0]
+          },
+          {
+            name: 'Warlock Ornaments',
+            items: [0]
+          },
+          {
+            name: 'Warlock Flawless Ornaments',
+            items: [0]
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: 'Factions',
+    sets: [
+      {
+        name: 'Dead Orbit Arsenal',
+        description:
+          'Weapons and armor obtained by working with Dead Orbit in their mission to push beyond the solar system.',
+        small: true,
+        sections: [
+          {
+            name: 'Weapons',
+            items: [0]
+          },
+          {
+            name: 'Hunter Ornaments',
+            items: [0]
+          },
+          {
+            name: 'Titan Ornaments',
+            items: [0]
+          },
+          {
+            name: 'Warlock Ornaments',
+            items: [0]
+          },
+          {
+            name: 'Extras',
+            items: [0]
+          }
+        ]
+      },
+      {
+        name: 'New Monarchy Arsenal',
+        description:
+          'Weapons and armor obtained by working with New Monarchy to strengthen the Last City.',
+        small: true,
+        sections: [
+          {
+            name: 'Weapons',
+            items: [0]
+          },
+          {
+            name: 'Hunter Ornaments',
+            items: [0]
+          },
+          {
+            name: 'Titan Ornaments',
+            items: [0]
+          },
+          {
+            name: 'Warlock Ornaments',
+            items: [0]
+          },
+          {
+            name: 'Extras',
+            items: [0]
+          }
+        ]
+      },
+      {
+        name: 'Future War Cult Arsenal',
+        description:
+          'Weapons and armor obtained by working with Future War Cult to prepare for the wars to come.',
+        small: true,
+        sections: [
+          {
+            name: 'Weapons',
+            items: [0]
+          },
+          {
+            name: 'Hunter Ornaments',
+            items: [0]
+          },
+          {
+            name: 'Titan Ornaments',
+            items: [0]
+          },
+          {
+            name: 'Warlock Ornaments',
+            items: [0]
+          },
+          {
+            name: 'Extras',
+            items: [0]
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: 'Vendor',
+    sets: [
+      {
+        name: 'Mercury Gear',
+        description:
+          'Gear obtained by performing tasks for Brother Vance on Mercury.',
+        small: false,
+        sections: [
+          {
+            name: 'Weapons',
+            items: [0]
+          },
+          {
+            name: 'Hunter Armor',
+            items: [0]
+          },
+          {
+            name: 'Titan Armor',
+            items: [0]
+          },
+          {
+            name: 'Warlock Armor',
+            items: [0]
+          }
+        ]
+      },
+
+      {
+        name: 'Vanguard Tactician Gear',
+        description:
+          'Weapons and armor obtained by carrying out special operations for Zavala.',
+        sections: [
+          {
+            name: 'Weapons',
+            items: [0]
+          },
+          {
+            name: 'Hunter Ornaments',
+            items: [0]
+          },
+          {
+            name: 'Titan Ornaments',
+            items: [0]
+          },
+          {
+            name: 'Warlock Ornaments',
+            items: [0]
+          }
+        ]
+      },
+
+      {
+        name: 'Crucible Engram',
+        description: 'Rewards for your efforts in the Crucible.',
+        sections: [
+          {
+            name: 'Weapons',
+            items: [0]
+          },
+
+          {
+            name: 'Hunter Ornaments',
+            items: [0]
+          },
+          {
+            name: 'Titan Ornaments',
+            items: [0]
+          },
+          {
+            name: 'Warlock Ornaments',
+            items: [0]
+          }
+        ]
+      },
+
+      {
+        name: 'Gunsmith Arsenal',
+        description:
+          'Weapons obtained by increasing your standing with Banshee-44, the Gunsmith.',
+        sections: [
+          {
+            name: 'Weapons',
+            items: [0]
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: 'Other',
+    sets: [
+      {
+        name: 'Eververse, Season 2',
+        big: true,
+        sections: []
+      }
+    ]
+  }
+];
