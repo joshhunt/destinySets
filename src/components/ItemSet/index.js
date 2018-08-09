00545cb4d8f610aa4fc69fa225dae4e929ad8d37
Josh Hunt <josh@trtr.co>
2018-08-09 13:53:49 +0100

    Style tweaks for hidden sets

--- src/components/ItemSet/index.js
+++ src/components/ItemSet/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import cx from 'classnames';
 
@@ -22,76 +22,110 @@ const LAZY_LOAD = true;
 
 const LazyLoad = LAZY_LOAD ? TheRealLazyLoad : ({ children }) => children;
 
-export function ItemSet({ className, setPopper, setModal, set, setHiddenItemSet }) {
-  const { name, id, noUi, description, sections, image, hidden } = set;
+class ItemSet extends Component {
+  toggleHidden = () => {
+    const {
+      setHiddenItemSet,
+      set: { id, hidden }
+    } = this.props;
 
-  return (
-    <div className={cx(className, styles.root, noUi && styles.noUi)}>
-      <div className={styles.inner}>
-        {!noUi && (
-          <div className={styles.header}>
-            {image && (
-              <img
-                alt=""
-                className={styles.headerImage}
-                src={`https://www.bungie.net${image}`}
-              />
-            )}
-            <div className={styles.headerText}>
-              <div className={styles.split}>
-              <div className={styles.splitMain}>
-                <h3 className={styles.title}>{name}</h3>
-              </div>
-              <div className={styles.headerAccessory}  onClick={() => {ls.saveHiddenItemSets(id,!hidden); setHiddenItemSet(id, !hidden);}}>
-                <Icon name={(hidden ? "plus" : "minus") + "-square"} />
-              </div>
-            </div>
-              {description && <p className={styles.desc}>{description}</p>}
-            </div>
-          </div>
+    ls.saveHiddenItemSets(id, !hidden);
+    setHiddenItemSet(id, !hidden);
+  };
+
+  render() {
+    const { className, setPopper, setModal, set } = this.props;
+    const { name, noUi, description, sections, image, hidden } = set;
+
+    return (
+      <div
+        className={cx(
+          className,
+          styles.root,
+          noUi && styles.noUi,
+          hidden && styles.hidden
         )}
-
-        {sections.map((section, index) => (
-          <LazyLoad height={85}>
-            <div key={index} className={styles.section}>
-              {!noUi && (
-                <h4 className={styles.sectionName}>
-                  {section.name}{' '}
-                  {section.season && (
-                    <span className={styles.seasonLabel}>
-                      S{section.season}
-                    </span>
-                  )}
-                </h4>
+      >
+        <div className={styles.inner}>
+          {!noUi && (
+            <div className={styles.header}>
+              {image && (
+                <img
+                  alt=""
+                  className={styles.headerImage}
+                  src={`https://www.bungie.net${image}`}
+                />
               )}
-
-              <div className={styles.itemListWrapper}>
-                {section.itemGroups.map((itemList, index2) => (
-                  <div className={styles.itemList} key={index2}>
-                    {itemList.map(itemHash => {
-                      const ItemComponent =
-                        ITEM_TYPE_COMPONENTS[section.itemType] || Item;
-
-                      return (
-                        <ItemComponent
-                          itemHash={itemHash}
-                          key={itemHash}
-                          extended={section.bigItems}
-                          className={!section.type && styles.item}
-                          setPopper={setPopper}
-                          onItemClick={setModal}
-                        />
-                      );
-                    })}
+              <div className={styles.headerText}>
+                <div className={styles.split}>
+                  <div className={styles.splitMain}>
+                    <h3 className={styles.title}>{name}</h3>
                   </div>
-                ))}
+
+                  <div className={styles.headerAccessory}>
+                    <button
+                      className={styles.hiddenToggle}
+                      onClick={this.toggleHidden}
+                    >
+                      <Icon light name={hidden ? 'eye-slash' : 'eye'} />
+
+                      {true && (
+                        <span className={styles.hiddenToggleText}>
+                          {' '}
+                          {hidden ? 'Hidden' : 'Hide'}
+                        </span>
+                      )}
+                    </button>
+                  </div>
+                </div>
+
+                {description && <p className={styles.desc}>{description}</p>}
               </div>
             </div>
-          </LazyLoad>
-        ))}
+          )}
+
+          {sections.map((section, index) => (
+            <LazyLoad height={85}>
+              <div key={index} className={styles.section}>
+                {!noUi && (
+                  <h4 className={styles.sectionName}>
+                    {section.name}{' '}
+                    {section.season && (
+                      <span className={styles.seasonLabel}>
+                        S{section.season}
+                      </span>
+                    )}
+                  </h4>
+                )}
+
+                <div className={styles.itemListWrapper}>
+                  {section.itemGroups.map((itemList, index2) => (
+                    <div className={styles.itemList} key={index2}>
+                      {itemList.map(itemHash => {
+                        const ItemComponent =
+                          ITEM_TYPE_COMPONENTS[section.itemType] || Item;
+
+                        return (
+                          <ItemComponent
+                            itemHash={itemHash}
+                            key={itemHash}
+                            extended={section.bigItems}
+                            className={!section.type && styles.item}
+                            setPopper={setPopper}
+                            onItemClick={setModal}
+                          />
+                        );
+                      })}
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </LazyLoad>
+          ))}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 const mapDispatchToActions = {
