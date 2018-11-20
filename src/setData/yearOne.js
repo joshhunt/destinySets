f6119f4104fb7dc806924e008924ddbc436f6d72
Jakosaur <Jakosaur@users.noreply.github.com>
2018-11-20 19:28:26 +0000

    2nd Sleeper Node Shader

--- src/setData/yearOne.js
+++ src/setData/yearOne.js
@@ -999,8 +999,10 @@ export default ([
               1273131835, // Arctic Arsenalist
               1291068171, // Data Mine
               1273131832, // Warminded
+              235397500, // The Mad Monk 
               235397502, // Arctic Dreamscape
               235397503 // Bray Innovation
+              
             ]
           }
         ]
