797b12267aa027cb0b640d8002b0b931fe16e176
Jakosaur <jacobtammadge@live.co.uk>
2019-02-07 08:47:11 +0000

    -

--- src/setData/yearTwo.js
+++ src/setData/yearTwo.js
@@ -717,7 +717,7 @@ export default ([
             ]
           },
           {
-            name: "Eliminated Barons' Armor",
+            name: "Baron Exclusive Gear",
             season: 4,
             itemGroups: [
               [
