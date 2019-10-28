07757e72fd13ecbf05438b7606d49cad6a426d57
Josh Hunt <josh@trtr.co>
2019-10-28 18:28:20 +0000

    new catalysts

--- src/extraData/catalystTriumphs.js
+++ src/extraData/catalystTriumphs.js
@@ -11,33 +11,37 @@ export default {
   3844694310: 3812775859, // The Jade Rabbit
   2286143274: 1962727403, // The Huckleberry
   2856683562: 3121210389, // SUROS Regime
+  1541131350: 2136108437, // Cerberus+1
+  347366834: 2136108437, // Ace of Spades
   3211806999: 3248675632, // Izanagi's Burden
   400096939: 1576480714, // Outbreak Perfected
+  2816212794: 2245186555, // Bad JuJu
 
   // Energy weapons
+  4190156464: 2490851406, // Merciless
   1345867571: 495916021, // Coldheart
-  2208405142: 2087362844, // Telesto
-  2907129557: 267836607, // Sunshot
-  3141979347: 745021037, // Borealis
-  4124984448: 4243279770, // Hard Light
-  4255268456: 3985015094, // Skyburner's Oath
-  3089417789: 4181440763, // Riskrunner
-  3413074534: 1640774328, // Polaris Lance
   3549153978: 3250123968, // Fighting Lion
-  3549153979: 3715769785, // The Prospector
+  2907129557: 267836607, // Sunshot
   3628991658: 221857936, // Graviton Lance
+  4255268456: 3985015094, // Skyburner's Oath
+  3141979347: 745021037, // Borealis
+  3089417789: 4181440763, // Riskrunner
+  4124984448: 4243279770, // Hard Light
   19024058: 3885534828, // Prometheus Lens
+  2208405142: 2087362844, // Telesto
+  3413074534: 1640774328, // Polaris Lance
+  3524313097: 2369153435, // Eriana's Vow
 
   // Heavy weapons
+  3549153979: 3715769785, // The Prospector
   1508896098: 543481464, // The Wardcliff Coil
-  1891561814: 773877428, // Whisper of the Worm
-  1864563948: 2562666046, // Worldline Zero
   3580904581: 3337586115, // Tractor Cannon
   3580904580: 3337586114, // Legend of Acrius
-  4190156464: 2490851406, // Merciless
-  4036115577: 3686797699, // Sleeper Simulant
+  3141979346: 2747759652, // D.A.R.C.I.
   3899270607: 2493039085, // The Colony
-  3141979346: 2747759652 // D.A.R.C.I.
+  1864563948: 2562666046, // Worldline Zero
+  4036115577: 3686797699, // Sleeper Simulant
+  1891561814: 773877428 // Whisper of the Worm
 
   // 1364093401: 0, // The Last Word
   // 2694576561: 0, // Two-Tailed Fox
@@ -48,7 +52,6 @@ export default {
   // 3973202132: 0, // Thorn
   // 3588934839: 0, // Le Monarque
   // 2044500762: 0, // The Queenbreaker
-  // 1541131350: 0, // Cerberus+1
   // 1852863732: 0, // Wavesplitter
   // 2130065553: 0, // Arbalest
   // 2069224589: 0, // One Thousand Voices
@@ -58,6 +61,5 @@ export default {
   // 417164956: 0, // Jötunn
   // 814876685: 0, // Trinity Ghoul
   // 814876684: 0, // Wish-Ender
-  // 347366834: 0, // Ace of Spades
   // 204878059: 0, // Malfeasance
 };
